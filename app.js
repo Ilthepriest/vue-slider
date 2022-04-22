58856bf2ba76f8be475c7d80a37f76f655a36a8c
Ilthepriest <robertodelprete1993@libero.it>
2022-04-22 19:50:28 +0200

    correct

--- app.js
+++ app.js
@@ -24,11 +24,13 @@ const app = new Vue({
                 image: 'img/04.jpg',
                 title: 'Germania',
                 text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+                id: 3
             },
             {
                 image: 'img/05.jpg',
                 title: 'Paradise',
                 text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
+                id: 4
             }
         ]
     },
@@ -50,10 +52,5 @@ const app = new Vue({
             }
         },
 
-        clickSize(event){
-            
-            console.log(event.target);
-            
-        }
     }
 })
